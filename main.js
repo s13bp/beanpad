48300f39fd98b11757bac143caa996c2e2af517e
s13bp <86206902+s13bp@users.noreply.github.com>
2022-01-09 22:31:30 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3497,7 +3497,55 @@ _libs_constants__WEBPACK_IMPORTED_MODULE_2__.INNER_SUB_MENUS_BTN.forEach(functio
   element.addEventListener('click', function () {
     (0,_libs_slide__WEBPACK_IMPORTED_MODULE_1__.slideToggle)(element.nextElementSibling);
   });
-});
+}); // function([string1, string2],target id,[color1,color2])    
+
+consoleText(['A Launchpad with ZERO Token Supply Fee Options for DeFi.', 'Haha pinksale sucks', 'Made with Love.'], 'text', ['yellow', 'rebeccapurple', 'lightblue']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id);
+  target.setAttribute('style', 'color:' + colors[0]);
+  window.setInterval(function () {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function () {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0]);
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function () {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount);
+      letterCount += x;
+    }
+  }, 120);
+  window.setInterval(function () {
+    if (visible === true) {
+      con.className = 'console-underscore hidden';
+      visible = false;
+    } else {
+      con.className = 'console-underscore';
+      visible = true;
+    }
+  }, 400);
+}
 })();
 
 /******/ })()
